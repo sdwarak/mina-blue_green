08314c71f7a00ed27726476454bef5dcdd5ea361
Shashi Prabhakar <shashiip@yahoo.com>
2017-06-03 10:43:53 -0700

    Make it work with Mina 1.0
    Make it work with Mina 1.0

--- lib/mina/blue_green.rb
+++ lib/mina/blue_green.rb
@@ -1,5 +1,5 @@
-set_default :blue, 'CHANGE THIS'
-set_default :green, 'CHANGE THIS'
+# set_default :blue, 'CHANGE THIS'
+# set_default :green, 'CHANGE THIS'
 
 
 %w(blue green).each do |color|
@@ -8,17 +8,14 @@ set_default :green, 'CHANGE THIS'
   end
   desc "Set the target environment to '#{color}'."
   task(color) do
-    val = settings.send(color.to_sym)
-    if _color == color and val != 'CHANGE THIS'
-      set :domain, val
-    end
+    set :domain, fetch(color.to_sym)
   end
 end
 
 desc 'Blue Green status'
 task :status do
   isolate do
-    queue %{
+    command %{
       current_sha=$(git --git-dir=#{deploy_to}/scm describe --always HEAD)
       current_branch=$(git --git-dir=#{deploy_to}/scm branch --contains $current_sha | sed -n 1p)
       current_rails_env=$(ps auxf | grep unicorn | sed -n 1p | sed 's/.*-E //' | awk '{print $1}')
